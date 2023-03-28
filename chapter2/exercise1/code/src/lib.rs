695749f9f4ee8e95c9faddd1a1e055aadeb6874e
wire3 <dw3@tutanota.com>
2023-03-28 16:20:16 +0200

    Second push

--- chapter2/exercise1/code/src/lib.rs
+++ chapter2/exercise1/code/src/lib.rs
@@ -10,34 +10,33 @@ use scrypto::prelude::*;
 enum Vegetable {
     Tomato,
     Carrot,
-    Broccoli
+    Broccoli,
 }
 
 #[blueprint]
 mod exercise_module {
     struct Exercise1 {
-        // Step 1: Add two variables here. 
+        // Step 1: Add two variables here.
         //   - One u64 named `instantiated_at`. This will contain the epoch at which the component was instantiated
         //   - One String named `instantiator_name`
-
+        let instantiated_at: u64;
+        let instantiator_name: String;
 
         // Step 5: Add a variable of type Vegetable named `favorite_vegetable`
-
-
+        let favorite_vegetable: Vegetable;
     }
 
     impl Exercise1 {
-
         // Step 2: add a String argument named `instantiator_name` to the following function
-        pub fn instantiate_exercise(  ) -> ComponentAddress {
+        pub fn instantiate_exercise(instantiator_name: String) -> ComponentAddress {
             Self {
                 // Step 3: Assign a value to the `instantiated_at` and `instantiator_name` variables.
                 // Help: You can get the current epoch with `Runtime::current_epoch()`
-
+                instantiated_at: Runtime::current_epoch();
+                instantiator_name: instantiator_name;
 
                 // Step 6: Assign a value to the `favorite_vegetable` variable
-
-
+                favorite_vegetable: Vegetable::Broccoli;
             }
             .instantiate()
             .globalize()
@@ -46,11 +45,13 @@ mod exercise_module {
         // Step 7: Create a `log_data` method that does the following:
         //   - Display the instantiator_name and instantiated_at variables using the `error!` macro
         //   - Display the favorite vegetable using the `debug!` macro
-        // Help: You can display formatted messages using the following notation: 
+        // Help: You can display formatted messages using the following notation:
         //   "blah {} blah" -> for instantiator_name and instantiated_at
         //   "blah {:?} blah" -> for the favorite_vegeratable. This is different notation than the other variables because
         //     the enum has a Debug implementation but not a Display one
-
-        
+        pub fn log_data(&self){
+            error!("blah {} blah {}", self.instantiator_name, self.instantiated_at);
+            debug!("blah {:?} blah", self.favorite_vegeratable);
+        }
     }
 }
